130a54dc1b1a5ed703b916d6627deb9525d1d875
Nikolay Nikitin <takry17@gmail.com>
2022-08-03 08:00:48 +0300

    4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'authapp.ShopUser'

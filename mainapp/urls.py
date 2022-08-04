3891df2391b3534bce5c48ad556166628c1afe39
Nikolay Nikitin <takry17@gmail.com>
2022-08-04 08:39:24 +0300

    6

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from mainapp.views import products
+from mainapp.views import products, product
+
 app_name = 'products'
 urlpatterns = [
     path('', products, name='products_hot_product'),
     path('<int:pk>', products, name='product_list'),
-
+    path('product/<int:pk>/', product, name='product_item'),
     ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,13 +3,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from basketapp.models import Basket
 from mainapp.models import Product, Category
-
-
-def get_basket(user):
-    basket_list = 0
-    if user.is_authenticated:
-        basket_list = sum(list(Basket.objects.filter(user=user).values_list('quantity', flat=True)))
-    return basket_list
+from mainapp.services import get_basket, get_hot_product, get_same_products
 
 
 def index(request):
@@ -38,17 +32,30 @@ def products(request, pk=None):
             'basket': get_basket(request.user),
         }
         return render(request, 'mainapp/products_list.html', context)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     context = {
         'links_menu': links_menu,
         'basket': get_basket(request.user),
-        'hot_product': Product.objects.order_by('?').first(),
-        'same_products': Product.objects.all().order_by('?')[3:5]
+        'hot_product': hot_product,
+        'same_products': same_products,
     }
     return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    context ={
+    context = {
         'basket': get_basket(request.user),
     }
     return render(request, 'mainapp/contact.html', context)
+
+
+def product(request, pk):
+    product_item = get_object_or_404(Product, pk=pk)
+    context = {
+        'links_menu': Category.objects.all(),
+        'product': product_item,
+        'basket': get_basket(request.user),
+
+    }
+    return render(request, 'mainapp/product.html', context)
